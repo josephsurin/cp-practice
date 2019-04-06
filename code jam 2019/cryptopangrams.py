e8941a37831819616e9800576f1969016b605b35
Joseph Surin <joseph.surin@gmail.com>
2019-04-07 08:39:16 +1000

    cryptopangrams

--- code jam 2019/cryptopangrams.py
+++ code jam 2019/cryptopangrams.py
@@ -14,23 +14,25 @@ for tc in range(1, T+1):
 			pf2 = int(minpp3//i)
 			break
 		i+=1
-	if minn == 0:
-		if (c[minn+3]/pf1 == c[minn+3]//pf1) and (c[minn+3]/pf2 != c[minn+3]//pf2):
-			p[minn+3] = pf1
-			p[minn] = pf2
-		else:
-			p[minn+3] = pf2
-			p[minn] = pf1
-	if (c[minn-1]/pf1 == c[minn-1]//pf1) and (c[minn-1]/pf2 != c[minn-1]//pf2):
-		p[minn] = pf1
-		p[minn+3] = pf2
-	elif (c[minn-1]/pf2 == c[minn-1]//pf2):
-		p[minn] = pf2
+	try:
 		p[minn+3] = pf1
-	for i in range(minn, 0, -1):
-		p[i-1] = c[i-1]//p[i]
-	for i in range(minn, len(p)-1):
-		p[i+1] = c[i]//p[i]
-	sp = sorted(set(p))
-	k = dict(zip(sp, alph))
-	print("Case #{}: {}".format(tc, ''.join([k[a] for a in p])))
+		p[minn] = pf2
+		for i in range(minn, 0, -1):
+			if c[i-1]/p[i] != c[i-1]//p[i]: raise ""
+			p[i-1] = c[i-1]//p[i]
+		for i in range(minn, len(p)-1):
+			if c[i]/p[i] != c[i]//p[i]: raise ""
+			p[i+1] = c[i]//p[i]
+		sp = sorted(set(p))
+		k = dict(zip(sp, alph))
+		print("Case #{}: {}".format(tc, ''.join([k[a] for a in p])))
+	except:
+		p[minn+3] = pf2
+		p[minn] = pf1
+		for i in range(minn, 0, -1):
+			p[i-1] = c[i-1]//p[i]
+		for i in range(minn, len(p)-1):
+			p[i+1] = c[i]//p[i]
+		sp = sorted(set(p))
+		k = dict(zip(sp, alph))
+		print("Case #{}: {}".format(tc, ''.join([k[a] for a in p])))
